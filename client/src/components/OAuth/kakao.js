86e0f294c13f1a1e017773541e09b8f4edd09ada
Jaekomplett <wanzekim@gmail.com>
2022-02-26 19:45:15 +0900

    Fix social url

--- client/src/components/OAuth/kakao.js
+++ client/src/components/OAuth/kakao.js
@@ -1,5 +1,2 @@
-const REDIRECT_URI = "http://127.0.0.1:5500/client/public/index.html";
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${REDIRECT_URI}/kakao`;
-
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}/kakao`;
 //카카오 로그인 버튼에 href로 링크걸어서 연결해주기
