043878421f57be38406320792c87780a578ec21d
Lee Seung-Seop <latte47s@naver.com>
2022-03-22 01:50:02 +0900

    Fixed postAddress 등록btn

--- client/src/components/Map/PostAddress.js
+++ client/src/components/Map/PostAddress.js
@@ -1,56 +1,43 @@
 import React, { useEffect, useState } from "react";
 import DaumPostcode from "react-daum-postcode";
-import PostCardForm from "../PostCard/NewPostCard/PostCardForm";
 import "./PostAddress.scss";
 
 const PostAddress = (props) => {
   const [isAddress, setIsAddress] = useState("");
-  const [isZoneCode, setIsZoneCode] = useState();
   const [isPostOpen, setIsPostOpen] = useState(false);
-  const { onClose } = props;
+  const { onChange, onClose } = props;
 
   const handleComplete = (data) => {
-    const addProps = data;
     let fullAddress = data.address;
     localStorage.setItem("address", fullAddress);
-    // let extraAddress = "";
-
-    // if (data.addressType === "R") {
-    //   if (data.bname !== "") {
-    //     extraAddress += data.bname;
-    //   }
-    //   if (data.buildingName !== "") {
-    //     extraAddress +=
-    //       extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
-    //   }
-    //   fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
-    setIsZoneCode(data.zonecode);
-    setIsPostOpen(true);
+    setIsPostOpen(!isPostOpen);
     setIsAddress(fullAddress);
   };
-  const test = localStorage.getItem("address");
-  useEffect(() => {
-    localStorage.getItem("address");
-  }, [test]);
 
   return (
     <div className="popWrap">
-      <div className="popContent">
-        <div className="btnPop-box">
-          <div
-            className="btnPop"
-            onClick={() => {
-              onClose();
-            }}
-          >
-            닫기
-          </div>
+      <div>
+        <div>
           <div className="input-content">
             <DaumPostcode
               className="postCodeStyle"
               autoClose
               onComplete={handleComplete}
-            ></DaumPostcode>
+            />
+          </div>
+          <div className={isAddress !== "" ? "btn-content" : null}>
+            {isAddress}
+          </div>
+          <div
+            className="btn-pop"
+            onClick={() => {
+              onChange((prevState) => {
+                return { ...prevState, located: isAddress };
+              });
+              onClose();
+            }}
+          >
+            등록
           </div>
         </div>
       </div>
