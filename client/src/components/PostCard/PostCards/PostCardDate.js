18dfd20ef9ef85a302d5b4c5815ca8c13efa49a1
Lee Seung-Seop <latte47s@naver.com>
2022-03-23 19:14:50 +0900

    Fixed new Date()값 수정

--- client/src/components/PostCard/PostCards/PostCardDate.js
+++ client/src/components/PostCard/PostCards/PostCardDate.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const PostCardDate = (props) => {
-  const day = props.date.toLocaleDateString("ko-kr");
-  const time = props.date.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-  const date = `${day} ${time}`;
+const PostCardDate = ({ meetDay }) => {
+  console.log("getday : ", meetDay);
+  const year = meetDay.slice(2, 4);
+  const month = meetDay.slice(5, 7);
+  const day = meetDay.slice(8, 10);
+  const hour = meetDay.slice(11, 13);
+  const min = meetDay.slice(14, 16);
+  const date = `${year}.${month}.${day} ${hour}:${min}`;
 
   return (
     <div>
