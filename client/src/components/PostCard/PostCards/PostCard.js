6c3f2b645d6745393f1e5487d0f19aef6ba505ec
Lee Seung-Seop <latte47s@naver.com>
2022-03-24 18:11:37 +0900

    Fixed props data

--- client/src/components/PostCard/PostCards/PostCard.js
+++ client/src/components/PostCard/PostCards/PostCard.js
@@ -1,42 +1,42 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import PostCardItem from "./PostCardItem";
 import NewPostCard from "../NewPostCard/NewPostCard";
-import { DUMMY_POSTCARDS } from "../../../resource/datas";
 import "./PostCard.scss";
+import CategoryFilter from "../PostCardRead/CategoryFilter";
 
-const PostCard = () => {
+const PostCard = ({ data }) => {
   const [postCards, setPostCards] = useState([]);
+  console.log("axios data", data);
 
   const addPostCardHandler = (postCard) => {
     setPostCards((prevPostCards) => {
       return [postCard, ...prevPostCards];
     });
   };
+
+  const postLists = data.map((postCard) => (
+    <PostCardItem
+      key={postCard._id}
+      id={postCard._id}
+      category={postCard.category}
+      description={postCard.description}
+      title={postCard.title}
+      date={postCard.date}
+      deliveryFee={postCard.deliveryFee}
+      totalNum={postCard.totalNum}
+      currentNum={postCard.currentNum}
+      located={postCard.located}
+      deliveryTag={postCard.deliveryTag}
+      payTag={postCard.payTag}
+    />
+  ));
+
   return (
     <div className="postCard">
+      <CategoryFilter />
       <NewPostCard onAddPostCard={addPostCardHandler} />
       <div className="postCardWrap">
-        <div className="postCards">
-          {data.map((postCard) => (
-            <PostCardItem
-              _id={postCard.id}
-              category={postCard.category}
-              description={postCard.description}
-              title={postCard.title}
-              date={postCard.date}
-              joinTime={postCard.joinTime}
-              deliveryFee={postCard.deliveryFee}
-              totalNum={postCard.totalNum}
-              currentNum={postCard.currentNum}
-              located={postCard.located}
-              latitude={postCard.latitude}
-              longitude={postCard.longitude}
-              deliveryTag={postCard.deliveryTag}
-              payTag={postCard.payTag}
-            />
-          ))}
-        </div>
+        <div className="postCards">{postLists}</div>
       </div>
     </div>
   );
