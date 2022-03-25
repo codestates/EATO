2a8b543b02a9b4d246897bbb95ea1ad91e44ffa0
Lee Seung-Seop <latte47s@naver.com>
2022-03-26 02:08:50 +0900

    Fixed time 2-digit

--- client/src/components/PostCard/PostCards/PostCardDate.js
+++ client/src/components/PostCard/PostCards/PostCardDate.js
@@ -2,11 +2,18 @@ import React from "react";
 
 const PostCardDate = ({ meetDay }) => {
   const strMeetDay = new Date(meetDay).toLocaleString("ko-Kr");
-  const date = strMeetDay.slice(2, 20);
+  const time = new Date(meetDay).toLocaleString("ko-KR", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = strMeetDay.slice(2, 12);
 
   return (
     <div>
-      <div>{date}</div>
+      <div>
+        {date} &nbsp;
+        {time}
+      </div>
     </div>
   );
 };
