34f54aa238d7d6bc6c85b7a5aea5c41f29ba174a
Lee Seung-Seop <latte47s@naver.com>
2022-03-25 22:05:19 +0900

    Fixed categoryOptions add key

--- client/src/resource/datas.js
+++ client/src/resource/datas.js
@@ -1,15 +1,15 @@
 export const categoryOptions = [
-  "한식",
-  "중식",
-  "양식",
-  "일식",
-  "분식",
-  "치킨",
-  "피자",
-  "카페·디저트",
-  "족발·보쌈",
-  "찜·탕 / 야식",
-  "기타",
+  { name: "한식" },
+  { name: "중식" },
+  { name: "양식" },
+  { name: "일식" },
+  { name: "분식" },
+  { name: "치킨" },
+  { name: "피자" },
+  { name: "카페·디저트" },
+  { name: "족발·보쌈" },
+  { name: "찜·탕 / 야식" },
+  { name: "기타" },
 ];
 
 export const deliTagName = [{ text: "배달" }, { text: "포장" }];
