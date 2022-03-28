091e65418be2f9b0e7b0564ff2f14d188e0138c9
Lee Seung-Seop <latte47s@naver.com>
2022-03-28 10:14:44 +0900

    Delete axios get

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,15 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import Nav from "../components/Nav/Nav";
 import HomePageSearchBar from "../components/SearchBar/HomePageSearchBar";
 import PostCard from "../components/PostCard/PostCards/PostCard";
 import Footer from "../components/Footer/Footer";
 
 const Home = () => {
-  const [postData, setPostData] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:3000/document").then((res) => {
-      const posts = res.data.documentList;
-      setPostData(posts);
-    });
-  }, []);
-
   return (
     <>
       <Nav />
       <HomePageSearchBar />
-      <PostCard data={postData} />
+      <PostCard />
       <Footer />
     </>
   );
